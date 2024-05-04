8a0ee41d6bbc45af03fad544ab793a1d3f4e852e
Vishakha0901 <vishakhalokare0901@gmail.com>
2024-05-05 02:03:03 +0530

    Application card and api integrated for data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import ApplicationCard from './Components/ApplicationCard';
+// import Filter from './Components/Filter';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+
+  const [data,setData]=useState({});
+  const [count,setCount]=useState(15);
+  function fetchData()
+  {
+   
+    return new Promise((resolve,reject)=>{
+
+  fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", 
+    {method: "POST", 
+    body:JSON.stringify({ "limit": count, "offset": count}),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+    }
+  ).
+  then((res)=> res.json()).
+  then((res)=>
+      // console.log(res),
+      
+      setData(res)
+
+      ).catch((err)=>reject(err))
+          
+      })
+    }
+
+     const handleInfiniteScroll= ()=>{
+
+      try{
+        if (window.innerHeight + document.documentElement.scrollTop+1 >= document.documentElement.scrollHeight) {
+          setCount(count+6)
+      }      }
+      catch(error){
+        console.log(error)
+      }
+     };
+    useEffect(()=>{
+      fetchData()
+    },[count])
+
+    useEffect(()=>{
+      window.addEventListener("scroll",handleInfiniteScroll);
+      return ()=> window.removeEventListener("scroll",handleInfiniteScroll);
+    })
+
+    return (
+      <div >
+       <ApplicationCard cardData={data}/>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/Components/ApplicationCard/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const ApplicationCard = (props) => {
+  const data = props.cardData.jdList;
+  return (
+    <div className={styles.gridContainer}>
+      {Array.isArray(data) &&
+        data.length > 0 &&
+        data.map((item) => (
+          <div className={styles.cardWrapper}>
+            <div className={styles.ellipseCard}>Posted 5 days ago</div>
+            <div style={{ display: "flex" }}>
+              <img
+                src={item.logoUrl}
+                alt="Company Logo"
+                className={styles.imgWrapper}
+              />
+              <div>
+                <div>{item.companyName}</div>
+                <div>{item.jobRole} Engineer</div>
+                <div>{item.location}</div>
+              </div>
+            </div>
+            <div>
+              Estimated Salary: ₹{`${item.minJdSalary}-${item.maxJdSalary}`} LPA
+            </div>
+            <div className={styles.aboutContainer}>
+            <div>About Company:</div>
+            <div id="text" className={styles.description}>{item.jobDetailsFromCompany}</div>
+            </div>
+            <div className={styles.minExpContainer}>
+            <div>Minimum Experiance</div>
+            <span>{item.minExp ? item.minExp : 8} years</span>
+            </div>
+
+            <div className={styles.buttonsContainer}>
+            <button className={styles.buttonWrapper}>Easy Apply</button>
+            <button
+              className={`${styles.buttonWrapper} ${styles.unlockButtonWrapper}`}
+            >
+              Unlock refferal asks
+            </button>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ApplicationCard;
